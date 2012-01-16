7844247a77645842deddb208a39483da0cf78672
Kenneth Love <kenneth@gigantuan.net>
2012-01-16 15:34:43 -0800

    meh?

--- djrill/admin.py
+++ djrill/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 
 from djrill.views import DjrillIndexView, DjrillSendersListView
 
-
-admin.site.register_view("djrill", DjrillIndexView.as_view(), "Djrill")
-admin.site.register_view("djrill/senders/list", DjrillSendersListView.as_view())
+admin.site.register_view("djrill/senders/", DjrillSendersListView.as_view())
+admin.site.register_view("djrill/", DjrillIndexView.as_view(), "Djrill")
