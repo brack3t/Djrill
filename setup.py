094b36c5f28c51c84fd088eb0e4ff57ff4df9d60
Chris Jones <chris@brack3t.com>
2012-02-28 15:42:57 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="djrill",
+    version="0.1.0",
+    description='',
+    long_description='',
+    keywords="django, mailchimp, mandrill, email, email backend",
+    author="Kenneth Love <kenneth@brack3t.com>, Chris Jones <chris@brack3t.com>",
+    author_email="kenneth@brack3t.com",
+    url="https://github.com/brack3t/Djrill/",
+    license="BSD",
+    packages=["djrill"],
+    zip_safe=False,
+    install_requires=["requests", "django"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+)
