008b6cfa362a32eca6b60df7247831959ebaf576
Rafael E. Belliard <belliard@gmail.com>
2012-11-13 17:05:50 -0400

    Provide a way to block admin.py when django-adminplus isn't loaded.

--- djrill/admin.py
+++ djrill/admin.py
@@ -5,18 +5,25 @@ from djrill.views import (DjrillIndexView, DjrillSendersListView,
                           DjrillAddSenderView, DjrillTagListView,
                           DjrillUrlListView)
 
-admin.site.register_view("djrill/senders/", DjrillSendersListView.as_view(),
-    "djrill_senders", "senders")
-admin.site.register_view("djrill/status/", DjrillIndexView.as_view(),
-    "djrill_status", "status")
-admin.site.register_view("djrill/tags/", DjrillTagListView.as_view(),
-    "djrill_tags", "tags")
-admin.site.register_view("djrill/urls/", DjrillUrlListView.as_view(),
-    "djrill_urls", "urls")
+''' 
+This condition is set so wild 'admin.autodiscovers' don't call this admin.py 
+when we don't want it to and when django-adminplus isn't set, which is 
+colossally breaks the rest of the admins' sections.
+'''
+if hasattr(admin.site,'register_view'):
+    admin.site.register_view("djrill/senders/", DjrillSendersListView.as_view(),
+                             "djrill_senders", "senders")
+    admin.site.register_view("djrill/status/", DjrillIndexView.as_view(),
+						 	 "djrill_status", "status")
+    admin.site.register_view("djrill/tags/", DjrillTagListView.as_view(),
+							 "djrill_tags", "tags")
+    admin.site.register_view("djrill/urls/", DjrillUrlListView.as_view(),
+							 "djrill_urls", "urls")
 
-admin.site.register_url("djrill/disable/sender/",
-    DjrillDisableSenderView.as_view(), "djrill_disable_sender")
-admin.site.register_url("djrill/verify/sender/",
-    DjrillVerifySenderView.as_view(), "djrill_verify_sender")
-admin.site.register_url("djrill/add/sender/",
-    DjrillAddSenderView.as_view(), "djrill_add_sender")
+    admin.site.register_url("djrill/disable/sender/", DjrillDisableSenderView.as_view(), 
+							"djrill_disable_sender")
+    admin.site.register_url("djrill/verify/sender/",  DjrillVerifySenderView.as_view(), 
+							"djrill_verify_sender")
+    admin.site.register_url("djrill/add/sender/",	DjrillAddSenderView.as_view(), 
+							"djrill_add_sender")
+
