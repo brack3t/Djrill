8a0fccdf53e4598c27589db672514c17bcfde1da
medmunds <medmunds@gmail.com>
2013-01-03 10:16:19 -0800

    setup: pull long_description and license from README and LICENSE files.
    Ensures info on PyPI matches version being distributed there. (Avoids problem where docs on github are ahead of published version on PyPI.)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Kenneth Love <kenneth@brack3t.com>, Chris Jones <chris@brack3t.com>",
     author_email="kenneth@brack3t.com",
     url="https://github.com/brack3t/Djrill/",
-    license="BSD",
+    license=open('LICENSE').read(),
     packages=["djrill"],
     zip_safe=False,
     install_requires=["requests", "django"],
@@ -22,14 +22,5 @@ setup(
         "Framework :: Django",
         "Environment :: Web Environment",
     ],
-    long_description="""\
-Djrill is an email backend for Django users who want to take advantage of the
-`Mandrill <http://mandrill.com>`_ transactional email service from MailChimp.
-
-In general, Djrill "just works" with Django's built-in ``django.core.mail``
-package. You can also take advantage of Mandrill-specific features like tags,
-metadata, and tracking. An optional Django admin interface is included.
-
-Full details are on the `project page <https://github.com/brack3t/Djrill>`_.
-""",
+    long_description=open('README.rst').read(),
 )
