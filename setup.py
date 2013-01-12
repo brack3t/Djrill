86b9711f2c96c14b782565d4b5a7b86af4c711c2
medmunds <medmunds@gmail.com>
2013-01-12 13:49:35 -0800

    setup: close license, readme files

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+with open('LICENSE') as file:
+    license_text = file.read()
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name="djrill",
     version="0.2.0",
@@ -8,7 +13,7 @@ setup(
     author="Kenneth Love <kenneth@brack3t.com>, Chris Jones <chris@brack3t.com>",
     author_email="kenneth@brack3t.com",
     url="https://github.com/brack3t/Djrill/",
-    license=open('LICENSE').read(),
+    license=license_text,
     packages=["djrill"],
     zip_safe=False,
     install_requires=["requests", "django"],
@@ -22,5 +27,5 @@ setup(
         "Framework :: Django",
         "Environment :: Web Environment",
     ],
-    long_description=open('README.rst').read(),
+    long_description=long_description,
 )
