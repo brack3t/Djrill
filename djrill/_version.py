551de88604db61f26ef03a8bbf74a37b6a67107c
medmunds <medmunds@gmail.com>
2013-10-20 10:50:30 -0700

    Start 0.8 development
    [ci skip]

--- djrill/_version.py
+++ djrill/_version.py
@@ -1,3 +1,3 @@
-VERSION = (0, 7, 0, 'dev1')  # Remove the 'dev1' component in release branches
+VERSION = (0, 8, 0, 'dev1')  # Remove the 'dev1' component in release branches
 __version__ = '.'.join([str(x) for x in VERSION])
 __minor_version__ = '.'.join([str(x) for x in VERSION[:2]])  # Sphinx's X.Y "version"
