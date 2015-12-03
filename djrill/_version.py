48028ecf649f6bf7e295ea4b6451d863c07c5a90
medmunds <medmunds@gmail.com>
2015-12-02 19:50:28 -0800

    Switch back to python-approved versioning
    2.0.0-dev0 --> 2.0.0.dev0 (or just 2.0.dev0)
    
    We still practice semver in the major.minor.patch
    versions; we just don't use semver's "-" for
    pre-release versions. (Because, PEP 0440.)
    
    https://www.python.org/dev/peps/pep-0440/

--- djrill/_version.py
+++ djrill/_version.py
@@ -1,5 +1,3 @@
-VERSION = (2, 0, 0, 'dev')  # Remove the 'dev' component in release branches
-__version__ = '.'.join([str(x) for x in VERSION[:3]])  # major.minor.patch
-if len(VERSION) > 3:  # x.y.z-pre.release (note the hyphen)
-    __version__ += '-' + '.'.join([str(x) for x in VERSION[3:]])
+VERSION = (2, 0, 'dev0')  # Remove the 'dev' component in release branches
+__version__ = '.'.join([str(x) for x in VERSION])  # major.minor.patch or major.minor.devN
 __minor_version__ = '.'.join([str(x) for x in VERSION[:2]])  # Sphinx's X.Y "version"
